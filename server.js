0fabe01b2a236181a6465765c859f1ec3451a3ec
aman <amanshukla18.as@gmail.com>
2020-07-06 16:04:47 +0530

    update eslint configuration and .gitignore filw

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => { res.json({ hello: 'Hello World!' }); });
-app.listen(port, () => { console.log('Example app listening at http://localhost:'); console.log('amsndfd'); });
+app.get("/", (req, res) => {
+  res.json({ hello: "Hello World!" });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
